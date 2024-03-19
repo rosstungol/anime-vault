9d5bb5f9a4e6feee3e40736f994ef2ee567f91ef
Ross Tungol <ross@proudcloud.io>
2024-03-19 17:56:43 +0800

    Implements inifinite scrolling in LoadMore component

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,21 +1,46 @@
-import Image from "next/image";
+"use client"
+
+import { useState, useEffect } from "react"
+import Image from "next/image"
+import { useInView } from "react-intersection-observer"
+import { fetchAnime } from "@/app/action"
+import AnimeCard, { AnimeProp } from "./AnimeCard"
+
+let page = 2
 
 function LoadMore() {
+  const { ref, inView } = useInView()
+  const [data, setData] = useState<AnimeProp[]>([])
+
+  useEffect(() => {
+    if (inView) {
+      fetchAnime(page).then((res) => {
+        setData([...data, ...res])
+        page++
+      })
+    }
+  }, [inView, data])
+
   return (
     <>
-      <section className="flex justify-center items-center w-full">
-        <div>
+      <section className='grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10'>
+        {data.map((item: AnimeProp, index: number) => (
+          <AnimeCard key={item.id} anime={item} index={index} />
+        ))}
+      </section>
+      <section className='flex justify-center items-center w-full'>
+        <div ref={ref}>
           <Image
-            src="./spinner.svg"
-            alt="spinner"
+            src='./spinner.svg'
+            alt='spinner'
             width={56}
             height={56}
-            className="object-contain"
+            className='object-contain'
           />
         </div>
       </section>
     </>
-  );
+  )
 }
 
-export default LoadMore;
+export default LoadMore
